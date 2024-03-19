a0f2455e376d8520b9603754bb20623dce7b1cfa
Park HyeJi <pgj0908@naver.com>
2024-03-19 12:15:27 +0900

    add p.22 code

--- 3_19_.py
+++ 3_19_.py
@@ -9,3 +9,11 @@ age_1=18
 age = int(input('your age? '))
 if age < age_0 and age > age_1:
     print("당신의 나이는 18~22세 사이입니다.")
+
+
+
+year = int(input("type a year : "))
+if(year%4==0 and year%100 !=0 )or(year%400)==0:
+    print(year, "is leap year")
+else:
+    print(year, "is not a leap year")
