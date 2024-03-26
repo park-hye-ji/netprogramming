1ef3695d1abd1986171511b3f23dbbb1a52deb3a
Park HyeJi <pgj0908@naver.com>
2024-03-26 10:28:55 +0900

    x 의 n제곱 구하기

--- 03_30.py
+++ 03_30.py
@@ -1,5 +1,8 @@
-sum =0
-for i in range(1,101,1):
-    sum=sum+i
+x=float(input('type x: '))
+n=int(input('tupe n: '))
 
-print("sum = ",sum)
+prod=1
+for i in range(1,n+1):
+    prod=prod*x
+
+print(prod)
