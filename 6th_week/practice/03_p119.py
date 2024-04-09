c9824b1cd85d19e664a23da332a5716ff2c37766
Park HyeJi <pgj0908@naver.com>
2024-04-09 12:42:02 +0900

    month dict

--- 6th_week/practice/03_p119.py
+++ 6th_week/practice/03_p119.py
@@ -1,9 +1,21 @@
-list=[]
-list2=[]
+days = {'January':31, 'February':28, 'March':31, 'April':30,'May':31, 'June':30, 'July':31, 'August':31, 'September':30, 'October':31, 'November':30,'December':31}
 
-for i in range(1,50):
-    list.append(i)
-    list2.append(i*i)
+month=input()
 
-print(list)
-print(list2)
+day=days.get(month)
+print(day)
+
+print(sorted(days))
+
+for i in days:
+    day=days.get(i)
+    if day==31:
+        print(i)
+
+print(sorted(days.items(), key=lambda t: t[1]))
+
+word_3=input("input month for 3 word: ")
+for i in days:
+    day=days.get(i)
+    if word_3 in i:
+        print(i)
