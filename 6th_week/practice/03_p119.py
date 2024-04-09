544995f0dec964b458c4a817c5159a2e8e4024a4
Park HyeJi <pgj0908@naver.com>
2024-04-09 12:19:37 +0900

    gcd

--- 6th_week/practice/03_p119.py
+++ 6th_week/practice/03_p119.py
@@ -1,6 +1,22 @@
-num = input("insert num")
+a = int(input("insert num1: "))
+b = int(input("insert num2: "))
 
-sum = 0
-for s in num :
-    sum +=int(s)
-    print(sum)
+def gcd(a,b):
+    if a>b:
+        while(b!=0):
+            a,b=b,a%b
+        return a
+    elif a<b:
+        while(a!=0):
+            b,a=a,b%a
+        return b
+    
+print(gcd(a,b))
+
+
+
+
+
+
+
+        
